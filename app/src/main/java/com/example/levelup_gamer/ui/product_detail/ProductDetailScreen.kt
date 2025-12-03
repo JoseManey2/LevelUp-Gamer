0232f860ee02a29c6031deef5db73913b54f6ef6
JoseManey <jos.montenegro@duocuc.cl>
2025-12-02 21:08:10 -0300

    imgenes resuelto

--- app/src/main/java/com/example/levelup_gamer/ui/product_detail/ProductDetailScreen.kt
+++ app/src/main/java/com/example/levelup_gamer/ui/product_detail/ProductDetailScreen.kt
@@ -162,7 +162,7 @@ fun ProductDetailScreen(
                                     AsyncImage(
                                         model = ImageRequest.Builder(LocalContext.current)
                                             // URL DE PRUEBA (ZELDA)
-                                            .data("https://upload.wikimedia.org/wikipedia/en/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg")
+                                            .data(imageUrl)
                                             .crossfade(true)
                                             .build(),
                                         contentDescription = product.name,
